b0ce6497b7bb138d098c95abd99518f8a40f2000
luxuyang <1115120651@qq.com>
2023-04-19 13:26:01 +0800

    leetcode简单第四天

---
+++ leetcode/爬楼梯/Solution.java
@@ -0,0 +1,27 @@
+package leetcode.爬楼梯;
+
+/*假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？*/
+public class Solution {
+
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+
+    public int climbStairs2(int n) {
+        if (n <= 1)
+            return 1;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
